51ac095c4fdbe9b0035897cfe7d1528a5678d426
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-03-15 17:55:46 -0400

    set up index page

---
+++ index.js
@@ -0,0 +1,18 @@
+let json;
+
+/*function preload(){
+  let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBMQ-Ox7rl6lz9nimSCwC1IdZRvsqYosvI&location=39.0, -97.0&radius=50000&type=park";
+}*/
+
+function setup() {
+  createCanvas(200, 200);
+  print(json);
+
+
+  //let headline = json.getJSONObject("response").getJSONArray("docs").
+    // Grabbing a single headline from the results.
+  //  getJSONObject(0).getJSONObject("headline").getString("main");
+  background(255);
+  fill(0);
+  //text(headline, 10, 10, 180, 190);
+}
